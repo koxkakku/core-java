70634a36eccd08cde96661c9197f34d4a41109ec
Sharad Kumar Dutta <sharad.k.dutta@gmail.com>
2023-07-20 11:03:10 +0530

    Update MapSort.java

--- src/main/java/com/sharad/learn/corejava/basic/MapSort.java
+++ src/main/java/com/sharad/learn/corejava/basic/MapSort.java
@@ -3,12 +3,17 @@
  */
 package com.sharad.learn.corejava.basic;
 
-import java.util.Comparator;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 /**
- * @author XY57013
+ * @author koxkakku
  *
  */
 public class MapSort {
@@ -17,21 +22,38 @@ public class MapSort {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Map<String,Integer> avgMarks = new HashMap();
-		avgMarks.put("Sharad", 72);
-		avgMarks.put("Saurav", 84);
-		avgMarks.put("Samya", 68);
-		avgMarks.put("Suman", 85);
-		avgMarks.put("Suresh", 60);
-		avgMarks.put("Surya", 91);
-		avgMarks.put("Sun", 70);
+
+		Map<String, Integer> unsortedMap = new HashMap<>();
+		unsortedMap.put("Apple", 5);
+		unsortedMap.put("Orange", 3);
+		unsortedMap.put("Banana", 8);
+
+		Map<String, Integer> sortedMap = new TreeMap<>(unsortedMap);
+		System.out.println(sortedMap); // Output: {Apple=5, Banana=8, Orange=3}
+
+		Map<String, Integer> sortedMap1 = unsortedMap.entrySet().stream()
+		    .sorted(Map.Entry.comparingByKey()) // Sort by key (natural order)
+		    // .sorted(Map.Entry.comparingByKey(Comparator.reverseOrder())) // Sort by key (reverse order)
+		    // .sorted(Map.Entry.comparingByValue()) // Sort by value (natural order)
+		    // .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())) // Sort by value (reverse order)
+		    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (a, b) -> a, LinkedHashMap::new));
+
+		System.out.println(sortedMap1); // Output: {Apple=5, Banana=8, Orange=3}
+
 		
-		avgMarks.entrySet().forEach(System.out::println);
-		//avgMarks.entrySet().stream().sorted(Comparator.comparing(Map.Entry::getValue)).forEach(System.out::println);
-		avgMarks.computeIfAbsent("Sankalp",k->k.length());
-		avgMarks.compute("Sankalp", (k,v)->v==null?1:v+48);
-		avgMarks.computeIfPresent("Sankalp", (k,v)->v==null?null:v+48);
-		avgMarks.entrySet().forEach(System.out::println);
+		List<Map.Entry<String, Integer>> entryList = new ArrayList<>(unsortedMap.entrySet());
+		Collections.sort(entryList, Map.Entry.comparingByKey()); // Sort by key (natural order)
+		// Collections.sort(entryList, Map.Entry.comparingByKey(Comparator.reverseOrder())); // Sort by key (reverse order)
+		// Collections.sort(entryList, Map.Entry.comparingByValue()); // Sort by value (natural order)
+		// Collections.sort(entryList, Map.Entry.comparingByValue(Comparator.reverseOrder())); // Sort by value (reverse order)
+
+		Map<String, Integer> sortedMap2 = new LinkedHashMap<>();
+		for (Map.Entry<String, Integer> entry : entryList) {
+		    sortedMap2.put(entry.getKey(), entry.getValue());
+		}
+
+		System.out.println(sortedMap2); // Output: {Apple=5, Banana=8, Orange=3}
+
 	}
 
 }
