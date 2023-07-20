8e0ec92d3cb7b54a5cd6be028358b886f79c2519
Sharad Kumar Dutta <sharad.k.dutta@gmail.com>
2023-07-20 10:58:06 +0530

    Update MapDemo.java

--- src/main/java/com/sharad/learn/corejava/basic/MapDemo.java
+++ src/main/java/com/sharad/learn/corejava/basic/MapDemo.java
@@ -90,5 +90,20 @@ public class MapDemo {
 			collect.forEach(item -> System.out.println("Continent Name :" + item.getKey()));
 		}
 
+		Map<String,Integer> avgMarks = new HashMap();
+		avgMarks.put("Sharad", 72);
+		avgMarks.put("Saurav", 84);
+		avgMarks.put("Samya", 68);
+		avgMarks.put("Suman", 85);
+		avgMarks.put("Suresh", 60);
+		avgMarks.put("Surya", 91);
+		avgMarks.put("Sun", 70);
+		
+		avgMarks.entrySet().forEach(System.out::println);
+		avgMarks.computeIfAbsent("Sankalp",k->k.length());
+		avgMarks.compute("Sankalp", (k,v)->v==null?1:v+48);
+		avgMarks.computeIfPresent("Sankalp", (k,v)->v==null?null:v+48);
+		avgMarks.entrySet().forEach(System.out::println);
+
 	}
 }
